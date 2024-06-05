dfda68339eff84cf54720c4f3f567b6e3a887d6b
Chung Hak Ngor <chunghakngor@gmail.com>
2024-06-05 21:14:10 +1200

    [nvim] enable mouse and increase hardtime counter

--- nvim/.config/nvim/lua/plugins/hardtime.lua
+++ nvim/.config/nvim/lua/plugins/hardtime.lua
@@ -3,7 +3,8 @@ return {
     "m4xshen/hardtime.nvim",
     dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
     opts = {
-      max_count = 5,
+      max_count = 10,
+      disable_mouse = false,
     },
     lazy = false,
   },
