02a3e59772b6b68f8bc952b809d06f45f45bd0a0
Chung Hak Ngor <chunghakngor@gmail.com>
2024-10-13 04:04:25 +1100

    [nvim] update conform config

--- nvim/.config/nvim/lua/configs/conform.lua
+++ nvim/.config/nvim/lua/configs/conform.lua
@@ -1,13 +1,17 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    typescript = { "prettier" },
     css = { "prettier" },
     html = { "prettier" },
     python = { "autopep8" },
     yaml = { "prettier" },
+    javascript = { "prettier" },
+    javascriptreact = { "prettier" },
+    typescript = { "prettier" },
+    typescriptreact = { "prettier" },
+    json = { "prettier" },
+    jsonc = { "prettier" },
   },
-
   format_on_save = {
     timeout_ms = 500,
     lsp_fallback = true,
